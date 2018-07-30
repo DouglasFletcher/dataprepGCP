598c26bf1bb3fcc4ac22fcb3840c44f3ac08e1bc
Fletcher <fletchy_d@hotmail.com>
2018-07-30 11:47:31 +0200

    dfo: modify python script

--- dependencies.sh
+++ dependencies.sh
@@ -2,7 +2,7 @@
 pip install -r requirements.txt
 
 ## clone repository --> move to deployment script
-git clone https://github.com/DouglasFletcher/dataprepGCP.git
+#git clone https://github.com/DouglasFletcher/dataprepGCP.git
 
-## where code exists
-cd /dataprepGCP
+## where code exists --> move to deployment script
+#cd /dataprepGCP
