7c92f25b581c5842043d723eaee641bd0305ad10
Fletcher <dofletcher@deloitte.de>
2018-08-06 17:46:36 +0200

    dof: added xy coordinates to weather data

---
+++ bigquery/geocoding.py
@@ -0,0 +1,14 @@
+
+
+
+
+def geocodePoint(fileName, colXCord, colYCord);
+ 	"""
+    geocode cloud storage data with x,y coordinates to give geoid e.g. zipcode
+    """
+	# read file as pointer value
+
+	# go through each row in file and merge geoId, with new names
+
+	# override file with same reference
+
