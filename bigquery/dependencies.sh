b452178f4b4c18eeba40873f5a6f189448a25242
Fletcher <dofletcher@deloitte.de>
2018-08-07 00:59:05 +0200

    DFO: testing python3 to bash

--- bigquery/dependencies.sh
+++ bigquery/dependencies.sh
@@ -3,6 +3,9 @@
 ## dependencies
 pip install -r requirements.txt
 
+## geocoded libraries
+pip install --user censusgeocode==0.4.2
+
 ## clone repository --> move to deployment script
 #git clone https://github.com/DouglasFletcher/dataprepGCP.git
 
