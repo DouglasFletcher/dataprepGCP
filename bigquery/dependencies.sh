d016720e2bbfbdbfb9c109bf566573ffebd901fb
Fletcher <fletchy_d@hotmail.com>
2018-08-08 18:31:28 +0200

    dfo: update geocoding to cloud storage read

--- bigquery/dependencies.sh
+++ bigquery/dependencies.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 ## dependencies
-pip install -r requirements.txt
-
-## geocoded libraries
-sudo pip install censusgeocode==0.4.2
+sudo pip install -r requirements.txt
 
 ## clone repository --> move to deployment script
 #git clone https://github.com/DouglasFletcher/dataprepGCP.git
