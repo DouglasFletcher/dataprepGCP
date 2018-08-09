d7a80108ef8ad636c6d0a32179754d19f5709672
Fletcher <fletchy_d@hotmail.com>
2018-08-09 12:10:54 +0200

    DFO. udpate geocoding

--- bigquery/geocoding.py
+++ bigquery/geocoding.py
@@ -6,24 +6,25 @@ import time
 def geocodeOhneSpark(fileDir, fileName, colX, colY):
 	# file reference
 	fileRef = fileDir + fileName
-	outfile = open(fileRef + "_complete" + ".csv","w") 
+	outfile = open(fileRef + "_geocoded" + ".csv","w") 
 	with open(fileRef + ".csv", "r") as ins:
 		errorCnt = 0
 		for i, line in enumerate(ins):
+			lineClean = line.replace('\n', '') 
 			# logging
 			if i % 50 == 0:
 				print("number of geocoded rows: %s" % i)
 			# get getoid with coordinates
 			if i == 0:
-				xcordPos = line.split(",").index(colX)
-				ycordPos = line.split(",").index(colY)
-				outfile.write(line.replace('\n', '')+",GEOID\n")
+				xcordPos = lineClean.split(",").index(colX)
+				ycordPos = lineClean.split(",").index(colY)
+				outfile.write(lineClean+",GEOID\n")
 			else:
-				xcord = line.split(",")[xcordPos]
-				ycord = line.split(",")[ycordPos]
+				xcord = lineClean.split(",")[xcordPos]
+				ycord = lineClean.split(",")[ycordPos]
 				try:
 					result = cg.coordinates(x=xcord, y=ycord)
-					outfile.write(line.replace('\n', '')+","+result["2010 Census Blocks"][0]["GEOID"] + "\n")
+					outfile.write(lineClean+","+result["2010 Census Blocks"][0]["GEOID"] + "\n")
 				except ValueError:
 					print("skipping line %s" % i)
 					errorCnt += 1
@@ -36,8 +37,7 @@ if __name__ == '__main__':
 	t = time.clock()
 	cwd = os.getcwd()
 	geocodeOhneSpark(cwd + "/weatherdata/","output","lon","lat")
-	#geocodeOhneSpark(cwd + "/statmetadata/","output","longitude","latitude")
-	#geocodeOhneSpark(cwd + "/statmetadata/","output","longitude","latitude")
+	geocodeOhneSpark(cwd + "/statmetadata/","output","longitude","latitude")
 
 	print(format(time.clock()-t, ".2f"))
 
