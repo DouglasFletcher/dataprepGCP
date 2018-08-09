3dff33f58874318784099f7dd14108c99e77a083
Fletcher <dofletcher@deloitte.de>
2018-08-09 12:10:54 +0200

    DFO. udpate geocoding

--- bigquery/geocoding_run.sh
+++ bigquery/geocoding_run.sh
@@ -6,13 +6,11 @@ export PROJECT="denolte-showcase-qlikbigdata"
 
 # get working data
 gsutil cp gs://$BUCKET/tables/weatherdata/output.csv ./weatherdata/output.csv
-#gsutil cp $BUCKET/tables/statmetadata/output.csv ./statmetadata/output.csv
-#gsutil cp $BUCKET/tables/bikedata/output.csv ./bikedata/output.csv
+gsutil cp $BUCKET/tables/statmetadata/output.csv ./statmetadata/output.csv
 
 # geocode files
 python geocoding.py
 
 # save back to cloud
-gsutil cp ./weatherdata/output_complete.csv gs://$BUCKET/tables/weatherdata/output_complete.csv
-#gsutil cp ./statmetadata/output.csv $BUCKET/tables/statmetadata/output.csv
-#gsutil cp ./bikedata/output.csv $BUCKET/tables/bikedata/output.csv
+gsutil cp ./weatherdata/output_geocoded.csv gs://$BUCKET/tables/weatherdata/output_geocoded.csv
+gsutil cp ./statmetadata/output_geocoded.csv $BUCKET/tables/statmetadata/output_geocoded.csv
