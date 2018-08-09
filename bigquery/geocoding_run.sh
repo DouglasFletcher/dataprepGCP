7c12069936a1e9919c4110da2c88f9ccd7b62bfd
Fletcher <dofletcher@deloitte.de>
2018-08-09 11:20:53 +0200

    DFO: update geocoding using gsutil

--- bigquery/geocoding_run.sh
+++ bigquery/geocoding_run.sh
@@ -5,7 +5,7 @@ export BUCKET="denolte-showcase-qlikbigdata"
 export PROJECT="denolte-showcase-qlikbigdata"
 
 # get working data
-gsutil cp $BUCKET/tables/weatherdata/output.csv ./weatherdata/output.csv
+gsutil cp gs://$BUCKET/tables/weatherdata/output.csv ./weatherdata/output.csv
 #gsutil cp $BUCKET/tables/statmetadata/output.csv ./statmetadata/output.csv
 #gsutil cp $BUCKET/tables/bikedata/output.csv ./bikedata/output.csv
 
@@ -13,6 +13,6 @@ gsutil cp $BUCKET/tables/weatherdata/output.csv ./weatherdata/output.csv
 python geocoding.py
 
 # save back to cloud
-gsutil cp ./weatherdata/output_complete.csv $BUCKET/tables/weatherdata/output_complete.csv
+gsutil cp ./weatherdata/output_complete.csv gs://$BUCKET/tables/weatherdata/output_complete.csv
 #gsutil cp ./statmetadata/output.csv $BUCKET/tables/statmetadata/output.csv
 #gsutil cp ./bikedata/output.csv $BUCKET/tables/bikedata/output.csv
