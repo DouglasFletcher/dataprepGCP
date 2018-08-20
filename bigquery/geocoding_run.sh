a15b073d8165d55cb3527ffd21905e92bf077aeb
Dennis Nolte <denolte@deloitte.de>
2018-08-20 20:41:51 +0000

    Changed data import significantly.

--- bigquery/geocoding_run.sh
+++ bigquery/geocoding_run.sh
@@ -5,16 +5,20 @@ export BUCKET="denolte-showcase-qlikbigdata"
 export PROJECT="denolte-showcase-qlikbigdata"
 
 # remove geocoded files
-gsutil -m rm -rf gs://$BUCKET/tables/weathermeta/output_geocoded.csv
-gsutil -m rm -rf gs://$BUCKET/tables/statmetadata/output_geocoded.csv
+gsutil -m rm -rf gs://$BUCKET/tables/noaa_gsod_stations_geocoded/output.csv
+gsutil -m rm -rf gs://$BUCKET/tables/citybike_stations_geocoded/output.csv
 
 # get working data
-gsutil cp gs://$BUCKET/tables/weathermeta/output.csv ./weathermeta/output.csv
-gsutil cp gs://$BUCKET/tables/statmetadata/output.csv ./statmetadata/output.csv
+gsutil cp gs://$BUCKET/tables/noaa_gsod_stations/output.csv ./noaa_gsod_stations/output.csv
+gsutil cp gs://$BUCKET/tables/citybike_stations/output.csv ./citybike_stations/output.csv
 
 # geocode files
 python geocoding.py
 
 # save back to cloud
-gsutil cp ./weathermeta/output_geocoded.csv gs://$BUCKET/tables/weathermeta/output_geocoded.csv
-gsutil cp ./statmetadata/output_geocoded.csv gs://$BUCKET/tables/statmetadata/output_geocoded.csv
+gsutil cp ./noaa_gsod_stations/output_geocoded.csv gs://$BUCKET/tables/noaa_gsod_stations/output.csv
+gsutil cp ./citybike_stations/output_geocoded.csv gs://$BUCKET/tables/citybike_stations/output.csv
+
+# Clean working directory
+rm -rf ./noaa_gsod_stations
+rm -rf ./citybike_stations
